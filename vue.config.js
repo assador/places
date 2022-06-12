25486857034b2f7443dccb8f82f3e35968bb8706
Dmitry Sokolov <dmitry@scrofa-tridens.ru>
2022-06-12 11:28:42 +0300

    Exit Vue 2 compatibility mode.

--- vue.config.js
+++ vue.config.js
@@ -3,20 +3,4 @@ module.exports = {
 		proxy: '',
 	},
 	//	lintOnSave: false,
-	chainWebpack: config => {
-		config.resolve.alias.set('vue', '@vue/compat')
-		config.module
-			.rule('vue')
-			.use('vue-loader')
-			.tap(options => {
-				return {
-					...options,
-					compilerOptions: {
-						compatConfig: {
-							MODE: 2
-						}
-					}
-				}
-			})
-	}
 }
