cf83e6cef4d769c82bb2ab0e81621e34f1aa5dcf
Dmitry Sokolov <assador@gmail.com>
2025-04-28 20:32:48 +0300

    Change .gitignore.

--- src/shared/auth.js
+++ src/shared/auth.js
@@ -19,7 +19,6 @@ export const loginRoutine = (user, voc) => {
                     emitter.emit('logged');
                 }
         }
-        console.log(response);
     })
         .catch(e => {
         console.error(e);
