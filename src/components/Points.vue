7c7fb6fb8a4584a058e23c91dfb27a7da370442b
Dmitry Sokolov <assador@gmail.com>
2026-01-16 01:10:32 +0300

    Points component styling.

--- src/components/Points.vue
+++ src/components/Points.vue
@@ -1,36 +1,29 @@
 <template>
-	<div
-		:class="
-			'points folder margin_bottom ' +
-			(opened ? 'folder_opened' : 'folder_closed')
-		"
-	>
+	<div class="points folder margin_bottom">
 		<div class="folder-subs points-header">
-			<div
-				class="folder-button"
+			<h2
+				:class="'child-basiccolor ' + (opened ? 'icon-opened' : 'icon-closed')"
 				@click="opened = !opened"
 			>
-				<div class="folder-button__content">
-					<h2>{{ mainStore.t.i.captions.points }}</h2>
-				</div>
-				<div
-					v-if="type === 'temps'"
-					class="folder-button__control button-iconed icon"
-					:class="'icon-geomark-' + (!mainStore.tempsPlacemarksShow ? '0' : '1')"
-					:title="
-						(mainStore.tempsPlacemarksShow
-							? mainStore.t.i.hints.hide
-							: mainStore.t.i.hints.show
-						) + ' ' +
-						mainStore.t.i.hints.placemarksOnMap
-					"
-					@click="e => {
-						e.stopPropagation();
-						mainStore.tempsPlacemarksShow = 
-							!mainStore.tempsPlacemarksShow;
-					}"
-				/>
-			</div>
+				{{ mainStore.t.i.captions.points }}
+			</h2>
+			<div
+				v-if="type === 'temps'"
+				class="folder-button__control button-iconed icon"
+				:class="'icon-geomark-' + (!mainStore.tempsPlacemarksShow ? '0' : '1')"
+				:title="
+					(mainStore.tempsPlacemarksShow
+						? mainStore.t.i.hints.hide
+						: mainStore.t.i.hints.show
+					) + ' ' +
+					mainStore.t.i.hints.placemarksOnMap
+				"
+				@click="e => {
+					e.stopPropagation();
+					mainStore.tempsPlacemarksShow = 
+						!mainStore.tempsPlacemarksShow;
+				}"
+			/>
 			<div
 				v-if="type === 'temps'"
 				class="control-buttons"
@@ -57,69 +50,70 @@
 				/>
 			</div>
 		</div>
-		<div class="folder-subfolders">
-			<div class="points-list-buttons">
-				<button
-					v-if="type === 'temps'"
-					v-for="(temp, id) in mainStore.temps"
-					:class="temp === mainStore.currentTemp ? 'button-pressed' : ''"
-					@click="emitter.emit('choosePoint', {point: temp})"
-					@contextmenu="e => {
-						e.preventDefault();
-						emitter.emit('choosePoint', {
-							point: temp,
-							mode: mainStore.mode,
-						});
+		<div :class="
+			'points-list-buttons folder-subfolders ' +
+			(opened ? 'opened' : 'closed')
+		">
+			<button
+				v-if="type === 'temps'"
+				v-for="(temp, id) in mainStore.temps"
+				:class="temp === mainStore.currentTemp ? 'button-pressed' : ''"
+				@click="emitter.emit('choosePoint', {point: temp})"
+				@contextmenu="e => {
+					e.preventDefault();
+					emitter.emit('choosePoint', {
+						point: temp,
+						mode: mainStore.mode,
+					});
+				}"
+			>
+				<span>{{ Object.keys(mainStore.temps).indexOf(id) + 1 }}</span>
+				<span
+					class="button-iconed icon icon-cross-45"
+					:title="mainStore.t.i.hints.deleteTemp"
+					@click="e => {
+						e.stopPropagation();
+						mainStore.deleteTemp(id);
 					}"
-				>
-					<span>{{ Object.keys(mainStore.temps).indexOf(id) + 1 }}</span>
-					<span
-						class="button-iconed icon icon-cross-45"
-						:title="mainStore.t.i.hints.deleteTemp"
-						@click="e => {
-							e.stopPropagation();
-							mainStore.deleteTemp(id);
-						}"
-					/>
-				</button>
-				<button
-					v-else-if="type === 'tracks'"
-					v-for="
-						(point, index) of
-						mainStore.trackAllPointsArray(mainStore.currentTrack)
-					"
-					:class="
-						point.point === mainStore.currentTrackPoint
-							? 'button-pressed' : ''
-					"
-					@click="emitter.emit('choosePoint', {point: point.point})"
-					@contextmenu="e => {
-						e.preventDefault();
-						emitter.emit('choosePoint', {
-							point: point.point,
-							mode: mainStore.mode,
-						});
+				/>
+			</button>
+			<button
+				v-else-if="type === 'tracks'"
+				v-for="
+					(point, index) of
+					mainStore.trackAllPointsArray(mainStore.currentTrack)
+				"
+				:class="
+					point.point === mainStore.currentTrackPoint
+						? 'button-pressed' : ''
+				"
+				@click="emitter.emit('choosePoint', {point: point.point})"
+				@contextmenu="e => {
+					e.preventDefault();
+					emitter.emit('choosePoint', {
+						point: point.point,
+						mode: mainStore.mode,
+					});
+				}"
+			>
+				<span>{{ index + 1 }}</span>
+				<span
+					class="button-iconed icon icon-cross-45"
+					:title="mainStore.t.i.hints.deleteTrack"
+					@click="e => {
+						e.stopPropagation();
+						mainStore.deleteTrackPoint(
+							mainStore.currentTrack, point.point.id
+						);
 					}"
-				>
-					<span>{{ index + 1 }}</span>
-					<span
-						class="button-iconed icon icon-cross-45"
-						:title="mainStore.t.i.hints.deleteTrack"
-						@click="e => {
-							e.stopPropagation();
-							mainStore.deleteTrackPoint(
-								mainStore.currentTrack, point.point.id
-							);
-						}"
-					/>
-				</button>
-			</div>
+				/>
+			</button>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref, Ref } from 'vue';
+import { ref } from 'vue';
 import { useMainStore } from '@/stores/main';
 import { emitter } from '@/shared/bus';
 
@@ -136,16 +130,34 @@ const opened = ref(true);
 
 <style lang="scss" scoped>
 .points-header {
+	display: flex;
 	gap: 8px;
-	.folder-button {
-		margin-left: -2px;
+	align-items: center;
+	margin-bottom: 12px;
+	&::before {
+		margin-left: 0;
+		width: 1em;
+		margin-right: 8px;
+	}
+	& > * {
+		cursor: pointer;
+	}
+	.folder-button__description {
+		height: 44px;
+	}
+	h2 {
+		flex: 1 0 auto;
+		margin: 0;
 		&::before {
-			margin-left: 0;
+			display: inline-block;
 			width: 1em;
-			margin-right: 8px;
+			font-size: 1rem;
 		}
-		.folder-button__description {
-			height: 44px;
+		&.icon-opened::before {
+			content: '\25e4';
+		}
+		&.icon-closed::before {
+			content: '\25e2';
 		}
 	}
 	.control-buttons {
@@ -175,6 +187,9 @@ const opened = ref(true);
 			padding: 8px 6px;
 			line-height: 0;
 		}
+	}
+	&.closed {
+		display: none;
 	}
 }
 </style>
